259ac771d5c023a687dc87ccd834c07595a97bbd
Yao Peng Li <l5e2o5n@gmail.com>
2011-06-03 21:07:38 +1000

    Fix calculating problem.

--- Calculate_repayment.php
+++ Calculate_repayment.php
@@ -6,23 +6,22 @@ $interest=$_GET["interest"];
 $frequency=$_GET["frequency"];
 $rtype=$_GET["rtype"];
 
-
 $period=0;
-if ($frequency="Weekly"){
+if ($frequency=="Weekly"){
 	$period=52;
 	}
-else if ($frequency="Fortnightly"){
+else if ($frequency=="Fortnightly"){
 	$period=26;
 	}
-else if ($frequency="Monthly"){
+else if ($frequency=="Monthly"){
 	$period=12;
 	}
+
 $power=$mortgage*$period;
 $term_interest=$interest/($period*100);
 
 $payment=round($amount*$term_interest*pow((1+$term_interest),$power)/(pow((1+$term_interest),$power)-1));
 
 echo "Your ".strtolower($frequency)." mortgage repayment<br /><h2>$".$payment."</h2>";
-	
 
 ?>
