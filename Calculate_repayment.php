2dfa0b8e59978a63505ec773548506d6588554e6
Yao Peng Li <l5e2o5n@gmail.com>
2011-06-06 02:31:19 +1000

    Create the simple graph drawing function

--- Calculate_repayment.php
+++ Calculate_repayment.php
@@ -22,6 +22,13 @@ $term_interest=$interest/($period*100);
 
 $payment=round($amount*$term_interest*pow((1+$term_interest),$power)/(pow((1+$term_interest),$power)-1));
 
+/*$text = "{\n".'"amount": "'.$amount.'",'."\n".'"mortgage": "'.$mortgage.'",'."\n".'"interest": "'.$interest.'",'."\n".'"frequency": "'.$frequency.'",'."\n".'"rtype": "'.$rtype.'",'."\n".'"payment": "'.$payment.'"'."\n}";
+
+file_put_contents('Result.json', $text);
+*/
+
+
+
 echo "Your ".strtolower($frequency)." mortgage repayment<br /><h2>$".$payment."</h2>";
 
 ?>
